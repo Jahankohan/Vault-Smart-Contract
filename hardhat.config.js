a4439284f01c59d84a281cf79fd01dee3da8048b
Reza Jahankohan <reza@Rezas-MacBook-Pro.local>
2024-05-13 10:32:50 +0300

    required packages updated alongside the env example

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,20 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY],
+      wethAddress: process.env.SEPOLIA_WETH_ADDRESS
+    },
+    holesky: {
+      url: process.env.HOLESKY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      wethAddress: process.env.HOLESKY_WETH_ADDRESS
     }
+  },
+  etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
